1404532858624cbead910660bbcb8cc9beaec65e
Matheus Ale <m.matheus.ale@gmail.com>
2021-12-18 14:26:29 -0300

    save travel dates

--- src/common/components/date-picker/index.tsx
+++ src/common/components/date-picker/index.tsx
@@ -6,11 +6,18 @@ import {
   FocusedInputShape,
   DateRangePickerShape,
 } from 'react-dates'
-import {useState} from 'react'
+import {useEffect, useState} from 'react'
 
-export const DatePicker = () => {
+export type DateSlice = [string | null, string | null]
+interface DatePickerProps {
+  value: DateSlice
+  onChange(dates: DateSlice): void
+}
+
+export const DatePicker = ({value, onChange}: DatePickerProps) => {
   const [startDate, setStartDate] = useState<moment.Moment | null>(null)
   const [endDate, setEndDate] = useState<moment.Moment | null>(null)
+
   const [focusedInput, setFocusedInput] = useState<FocusedInputShape | null>(
     null,
   )
@@ -19,21 +26,32 @@ export const DatePicker = () => {
     startDate,
     endDate,
   }) => {
+    const start = startDate?.utc().startOf('day').toISOString() || null
+    const end = endDate?.utc().endOf('day').toISOString() || null
     setStartDate(startDate)
     setEndDate(endDate)
+
+    onChange([start, end])
   }
 
+  useEffect(() => {
+    const [start, end] = value
+    if (start) setStartDate(moment(start))
+    if (end) setEndDate(moment(end))
+  }, [value])
+
   return (
-    <div>
-      <DateRangePicker
-        startDate={startDate} // momentPropTypes.momentObj or null,
-        startDateId="your_unique_start_date_id" // PropTypes.string.isRequired,
-        endDate={endDate} // momentPropTypes.momentObj or null,
-        endDateId="your_unique_end_date_id" // PropTypes.string.isRequired,
-        onDatesChange={onDatesChange} // PropTypes.func.isRequired,
-        focusedInput={focusedInput} // PropTypes.oneOf([START_DATE, END_DATE]) or null,
-        onFocusChange={setFocusedInput} // PropTypes.func.isRequired,
-      />
-    </div>
+    <DateRangePicker
+      startDate={startDate} // momentPropTypes.momentObj or null,
+      startDateId="your_unique_start_date_id" // PropTypes.string.isRequired,
+      endDate={endDate} // momentPropTypes.momentObj or null,
+      endDateId="your_unique_end_date_id" // PropTypes.string.isRequired,
+      onDatesChange={onDatesChange} // PropTypes.func.isRequired,
+      focusedInput={focusedInput} // PropTypes.oneOf([START_DATE, END_DATE]) or null,
+      onFocusChange={setFocusedInput} // PropTypes.func.isRequired,
+      withFullScreenPortal
+      appendToBody
+      disableScroll
+    />
   )
 }
--- src/pages/planer/timeline/index.tsx
+++ src/pages/planer/timeline/index.tsx
@@ -4,6 +4,7 @@ import {SliderRail, Handle, Track, Tick} from './components'
 const sliderStyle = {
   position: 'relative',
   width: '100%',
+  zIndex: 0,
 }
 
 const domain = [100, 500]
